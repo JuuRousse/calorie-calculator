ba137101066c286aa18b9a1e4befea96534557cc
Yuliia Truskova <ju.truskova@gmail.com>
2021-03-19 11:17:58 +0200

    Add local storage

--- script.js
+++ script.js
@@ -64,9 +64,42 @@ const app = document.querySelector('#app').innerHTML = `
 `;
 
 const result = document.querySelector('.result span');
-let sex = 'female',
-    height, weight, age,
+
+let sex, height, weight, age, ratio;
+
+
+if (localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex');
+} else {
+    sex = "female";
+    localStorage.setItem('sex', 'female');
+}
+
+if (localStorage.getItem('ratio')) {
+    sex = localStorage.getItem('ratio');
+} else {
     ratio = 1.375;
+    localStorage.setItem('ratio', 1.375);
+};
+
+const initLocalSettings = (selector, activeClass) => {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach(item => {
+        item.classList.remove(activeClass);
+
+        if (item.getAttribute('id') === localStorage.getItem('sex')) {
+            item.classList.add(activeClass);
+        }
+
+        if (item.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+            item.classList.add(activeClass);
+        }
+    })
+};
+
+initLocalSettings('#gender div', 'choose-item-active');
+initLocalSettings('.activity-selection div', 'choose-item-active');
 
 const calcTotal = () => {
     if (!sex || !height || !weight || !age || !ratio) {
@@ -82,15 +115,17 @@ const calcTotal = () => {
 
 calcTotal();
 
-const getStaticInformation = (parentSelector, activeClass) => {
-    const elements = document.querySelectorAll(`${parentSelector} div`);
+const getStaticInformation = (selector, activeClass) => {
+    const elements = document.querySelectorAll(selector);
 
     elements.forEach(item => {
         item.addEventListener('click', (event) => {
             if (event.target.getAttribute('data-ratio')) {
                 ratio = +event.target.getAttribute('data-ratio');
+                localStorage.setItem('ratio', +event.target.getAttribute('data-ratio'));
             } else {
                 sex = event.target.getAttribute('id');
+                localStorage.setItem('sex', event.target.getAttribute('id'));
             }
 
             elements.forEach(item => {
@@ -104,13 +139,14 @@ const getStaticInformation = (parentSelector, activeClass) => {
     });
 }
 
-getStaticInformation('#gender', 'choose-item-active');
-getStaticInformation('.activity-selection', 'choose-item-active');
+getStaticInformation('#gender div', 'choose-item-active');
+getStaticInformation('.activity-selection div', 'choose-item-active');
 
 const getDynamicInformation = (selector) => {
     const input = document.querySelector(selector);
 
     input.addEventListener('input', () => {
+
         switch (input.getAttribute('id')) {
             case "height":
                 height = +input.value;
